a8bdb73fa779f3f0ef8d2ad0f8dab273a629092f
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-03-26 19:09:27 +0000

    setup: add theme and fonts

---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import { Lalezar, Red_Rose } from "next/font/google";
+
+export const lalezar = Lalezar({ subsets: ["latin"], weight: ["400"] });
+export const red_rose = Red_Rose({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,105 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        foreground: "hsl(var(--foreground))",
+        background: "hsl(var(--background))",
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        tertiary: "hsl(var(--tertiary))",
+        quaternary: "hsl(var(--quaternary))",
+        quinary: "hsl(var(--quinary))",
+        "quinary-light": "hsl(var(--quinary-light))",
+        normal: {
+          DEFAULT: "hsl(var(--normal))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--normal-light))",
+        },
+        fighting: {
+          DEFAULT: "hsl(var(--fighting))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--fighting-light))",
+        },
+        flying: {
+          DEFAULT: "hsl(var(--flying))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--flying-light))",
+        },
+        poison: {
+          DEFAULT: "hsl(var(--poison))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--poison-light))",
+        },
+        ground: {
+          DEFAULT: "hsl(var(--ground))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--ground-light))",
+        },
+        rock: {
+          DEFAULT: "hsl(var(--rock))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--rock-light))",
+        },
+        bug: {
+          DEFAULT: "hsl(var(--bug))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--bug-light))",
+        },
+        ghost: {
+          DEFAULT: "hsl(var(--ghost))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--ghost-light))",
+        },
+        steel: {
+          DEFAULT: "hsl(var(--steel))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--steel-light))",
+        },
+        fire: {
+          DEFAULT: "hsl(var(--fire))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--fire-light))",
+        },
+        water: {
+          DEFAULT: "hsl(var(--water))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--water-light))",
+        },
+        grass: {
+          DEFAULT: "hsl(var(--grass))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--grass-light))",
+        },
+        electric: {
+          DEFAULT: "hsl(var(--electric))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--electric-light))",
+        },
+        psychic: {
+          DEFAULT: "hsl(var(--psychic))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--psyshic-light))",
+        },
+        ice: {
+          DEFAULT: "hsl(var(--ice))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--ice-light))",
+        },
+        dragon: {
+          DEFAULT: "hsl(var(--dragon))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--dragon-light))",
+        },
+        dark: {
+          DEFAULT: "hsl(var(--dark))",
+          foreground: "hsl(var(--background))",
+          light: "hsl(var(--dark-light))",
+        },
+        fairy: {
+          DEFAULT: "hsl(var(--fairy))",
+          foreground: "hsl(var(--foreground))",
+          light: "hsl(var(--fairy-light))",
+        },
       },
     },
   },
