259e8717d612b85303364fb63b6bd7258d859fed
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-03-31 17:20:14 +0100

    refactor: remove link from first and last indexes in navigation

--- app/components/ui/Navigation/Navigation.tsx
+++ app/components/ui/Navigation/Navigation.tsx
@@ -19,34 +19,51 @@ export default function Navigation({
   pokemonId,
 }: NavigationProps) {
   return (
-    <Link
-      href={`/${index}`}
-      className={clsx(
-        `${lalezar.className} flex w-6/12 uppercase text-xl md:text-2xl`,
-        navigationTheme(primaryType, direction)
-      )}
-    >
-      {direction === "left" ? (
-        <span className="flex justify-between sm:justify-evenly items-center gap-4 min-w-full px-4 sm:px-6">
-          <span className="bg-white p-1 sm:p-2 rounded-full">
-            <Icon icon="chevronLeft" width="12" height="12" />
-          </span>
-          <span className="flex -translate-y-2">
-            {index}&nbsp;
-            <span className="hidden sm:block">{formatId(pokemonId)}</span>
-          </span>
-        </span>
+    <>
+      {pokemonId > 151 || pokemonId < 1 ? (
+        <div
+          className={clsx(
+            `${lalezar.className} flex w-6/12 uppercase text-xl md:text-2xl`,
+            navigationTheme(primaryType, direction)
+          )}
+        >
+          {direction === "left" ? (
+            <span className="flex justify-between sm:justify-evenly items-center gap-4 min-w-full px-4 sm:px-6"></span>
+          ) : (
+            <span className="flex justify-between sm:justify-evenly items-center gap-4 min-w-full px-4 sm:px-6"></span>
+          )}
+        </div>
       ) : (
-        <span className="flex justify-between sm:justify-evenly items-center gap-4 min-w-full px-4 sm:px-6">
-          <span className="flex -translate-y-2">
-            {index}&nbsp;
-            <span className="hidden sm:block">{formatId(pokemonId)}</span>
-          </span>
-          <span className="bg-white p-1 sm:p-2 rounded-full">
-            <Icon icon="chevronRight" width="12" height="12" />
-          </span>
-        </span>
+        <Link
+          href={`/${index}`}
+          className={clsx(
+            `${lalezar.className} flex w-6/12 uppercase text-xl md:text-2xl transition`,
+            navigationTheme(primaryType, direction)
+          )}
+        >
+          {direction === "left" ? (
+            <span className="flex justify-between sm:justify-evenly items-center gap-4 min-w-full px-4 sm:px-6">
+              <span className="bg-white p-1 sm:p-2 rounded-full">
+                <Icon icon="chevronLeft" width="12" height="12" />
+              </span>
+              <span className="flex -translate-y-2">
+                {index}&nbsp;
+                <span className="hidden sm:block">{formatId(pokemonId)}</span>
+              </span>
+            </span>
+          ) : (
+            <span className="flex justify-between sm:justify-evenly items-center gap-4 min-w-full px-4 sm:px-6">
+              <span className="flex -translate-y-2">
+                {index}&nbsp;
+                <span className="hidden sm:block">{formatId(pokemonId)}</span>
+              </span>
+              <span className="bg-white p-1 sm:p-2 rounded-full">
+                <Icon icon="chevronRight" width="12" height="12" />
+              </span>
+            </span>
+          )}
+        </Link>
       )}
-    </Link>
+    </>
   );
 }
